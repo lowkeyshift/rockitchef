dbce0b75f2811b86227ff0335710b12e58115d36
ziquan <ziquan.miao@datadoghq.com>
2019-03-09 18:53:17 -0500

    edit recipe table to add title

--- db/create_tables.py
+++ db/create_tables.py
@@ -26,6 +26,7 @@ create_table_recipes = (
             "CREATE TABLE recipes("
             "id integer primary key autoincrement,"
             "chef_id integer,"
+            "title text",
             "url text,"
             "prep_time text,"
             "cook_time text,"
