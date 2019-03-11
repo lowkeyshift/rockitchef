468295e6dc69351a608b974b8fb6833089ad0cbf
Edwin Carrasquillo <edwin.carrasquillo@datadoghq.com>
2019-03-10 22:49:20 -0400

    updates to build-init script

--- build_script.sh
+++ build_script.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/env bash
 
+vDir="./rockitchef/"
+readonly sourceFile="webapp"
+FILE="./requirements.txt"
 # Check to make sure things are up to date.
 echo "====================================="
 echo "Updating Ubuntu"
 echo "====================================="
 
 sudo apt update > /dev/null 2>&1 && echo "Update complete..."
+
+# Writing requirements file for pip.
+echo "====================================="
+echo "Writing Requirements File."
+echo "====================================="
+/bin/cat <<EOM >$FILE
+beautifulsoup4==4.6.0
+fake-useragent==0.1.11
+Jinja2==2.10
+jsondiff==1.1.2
+PyYAML==3.12
+requests==2.21.0
+virtualenv==16.4.3
+EOM
 
 echo "====================================="
 echo "Starting PIP installs for rockitchef."
@@ -25,3 +42,23 @@ for package in "${PY_ARRAY[@]}"; do
         sudo -H pip3 install -r requirements.txt
     fi
 done
+
+echo "====================================="
+echo "Building VENV for rockitchef."
+echo "====================================="
+mkdir ${vDir}
+python3 -m venv ${vDir}${sourceFile}
+source ${vDir}${sourceFile}/bin/activate
+echo "===================================== \n"
+echo "${sourceFile} virtual environment is now running.\n"
+echo "====================================="
+echo "\n"
+echo "====================================="
+echo "Install Datadog."
+echo "====================================="
+export DD_API_KEY=094836a9dbf0f88f010fc33f3e087f3f
+bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
+sudo systemctl start datadog-agent
+echo "====================================="
+echo "Starting Datadog."
+echo "====================================="
