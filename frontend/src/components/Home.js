3199555c3a798c1a31e6ec07f1c0d684e3dad1a4
Ziquan Miao <ziquan.miao@datadoghq.com>
2019-06-20 17:58:01 -0400

    add recommendations page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,8 +3,16 @@ import { View, Button, StyleSheet } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import axios from 'axios';
 import List from './List';
-
+import Recommendations from './Recommendations';
 class Home extends Component {
+  constructor(props) {
+
+    super(props);
+    this.state = {
+      page:'home'
+    }
+
+  }
 
   handleRequest() {
     // This request will only succeed if the Authorization header
@@ -17,13 +25,35 @@ class Home extends Component {
       })
       .catch(error =>  console.log(error));
   }
-
+  setHomePage() {
+    this.setState({
+      page:'home'
+    })
+  }
+  setRecommendationPage() {
+    this.setState({
+      page:'recommendation'
+    })
+  }
+  showPage() {
+    if (this.state.page == 'home'){
+      return (
+        <List />
+      )
+    } else {
+      return (
+        <Recommendations />
+      )
+    }
+  }
   render() {
     const { buttonContainerStyle} = styles;
     return (
       <View style={buttonContainerStyle}>
+        <Button title="Inventory" onPress={this.setHomePage.bind(this)}/>
+        <Button title="Recommendations" onPress={this.setRecommendationPage.bind(this)}/>
         <Button title="Logout" onPress={this.handleRequest.bind(this)}/>
-        <List />
+        {this.showPage()}
       </View>
     );
   }
