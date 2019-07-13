e896c7a100107e6b827ae250a67face22cd5596b
Edwin Carrasquillo <edwin.carrasquillo@datadoghq.com>
2019-07-13 15:18:30 -0400

    updated the API register and login

--- docker_stack/recipes_app/views.py
+++ docker_stack/recipes_app/views.py
@@ -14,10 +14,15 @@ from .serializers import ChefSerializer
 from .serializers import DirectionSerializer
 from .serializers import IngredientSerializer
 from .serializers import CreateUserSerializer
+from .serializers import AuthCustomTokenSerializer
 
 from django.contrib.auth import get_user_model
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
+from rest_framework.parsers import JSONParser
+from rest_framework.parsers import FormParser
+from rest_framework.parsers import MultiPartParser
+from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
@@ -44,9 +49,39 @@ class CreateUserAPIView(CreateAPIView):
             headers=headers
         )
 
+class ObtainAuthToken(APIView):
+    throttle_classes = ()
+    permission_classes = ()
+    parser_classes = (
+        FormParser,
+        MultiPartParser,
+        JSONParser,
+    )
+
+    renderer_classes = (JSONRenderer,)
+
+    def post(self, request, *args, **kwargs):
+        serializer = AuthCustomTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        token_data = {"token": token.key}
+
+        return Response(
+            {**serializer.data, **token_data},
+            status=status.HTTP_201_CREATED,
+        )
 class ProfileView(viewsets.ModelViewSet):
-    queryset = Profile.objects.all()
+    #queryset = Profile.objects.all()
+    model = Profile
     serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+
+    def get_queryset(self):
+        return Profile.objects.filter(user=self.request.user)
+
+    def pre_save(self, obj):
+        obj.user = self.request.user
 
 class LogoutUserAPIView(APIView):
     queryset = get_user_model().objects.all()
@@ -54,7 +89,10 @@ class LogoutUserAPIView(APIView):
     def get(self, request, format=None):
         # simply delete the token to force a login
         request.user.auth_token.delete()
-        return Response(status=status.HTTP_200_OK)
+        return Response(
+        {"message": "Successfully Logged Out"},
+        status=status.HTTP_200_OK
+        )
 
 class ChefView(viewsets.ModelViewSet):
     queryset = Chef.objects.all()
