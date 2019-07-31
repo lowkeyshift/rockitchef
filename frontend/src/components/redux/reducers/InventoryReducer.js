04970aab2b0d264c170f7f08724b89aeb88a5953
Ziquan Miao <ziquan.miao@datadoghq.com>
2019-07-30 22:42:50 -0400

    add redux for deletion

--- frontend/src/components/redux/reducers/InventoryReducer.js
+++ frontend/src/components/redux/reducers/InventoryReducer.js
@@ -10,7 +10,13 @@ export const InventoryReducer = (
         ...state,
         inventory_items: [...state.inventory_items, action.itemName]
       };
-
+    case "DELETE_INVENTORY_ITEM":
+      return {
+        ...state,
+        inventory_items: inventory_items.filter(
+          item => item.name != action.itemName
+        )
+      };
     default:
       return state;
   }
